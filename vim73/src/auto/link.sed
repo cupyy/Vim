c5b79cdb89d26346475d741b4c4ea6dc576f0087
yyang <yyang@ffn.com>
2014-10-27 22:56:40 -0700

    initial commit

---
+++ vim73/src/auto/link.sed
@@ -0,0 +1,7 @@
+s/-lSM  *//
+s/-lSM  *//
+s/-lICE  *//
+s/-lICE  *//
+s/-lnsl  *//
+s/-lX11  *//
+s/-lXdmcp  *//
---
+++ vim74/src/auto/pathdef.c
@@ -0,0 +1,10 @@
+/* pathdef.c */
+/* This file is automatically created by Makefile
+ * DO NOT EDIT!  Change Makefile only. */
+#include "vim.h"
+char_u *default_vim_dir = (char_u *)"/usr/local/share/vim";
+char_u *default_vimruntime_dir = (char_u *)"";
+char_u *all_cflags = (char_u *)"gcc -c -I. -Iproto -DHAVE_CONFIG_H     -O2 -fno-strength-reduce -Wall -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=1      ";
+char_u *all_lflags = (char_u *)"gcc   -L/usr/local/lib -Wl,--as-needed -o vim    -lSM -lICE -lXt -lX11 -lXdmcp -lSM -lICE  -lm -lncurses -lnsl  -lselinux          ";
+char_u *compiled_user = (char_u *)"yyang";
+char_u *compiled_sys = (char_u *)"ii52-26.friendfinderinc.com";
